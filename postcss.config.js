027c0e26a62362f149c432bb43d664b3ef1fa3d4
Pierre Amelot <pierre.p.amelot@gmail.com>
2023-09-19 16:08:52 +0200

    Add support for modern css features
    * Ensure nesting plays nicely with tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   plugins: {
+    // https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
   },
-}
+};
